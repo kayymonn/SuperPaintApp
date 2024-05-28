e0f608ab53d4218d1e78b54570c5115e90ef9fea
adam vav <vavruskaa@outlook.com>
2024-05-28 23:07:17 +0200

    Add project files.

---
+++ SuperPaintApp/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace SuperPaintApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            Btn_clear = new Button();
+            Btn_load = new Button();
+            Btn_save = new Button();
+            Btn_line = new Button();
+            Btn_rect = new Button();
+            Btn_ellipse = new Button();
+            Btn_eraser = new Button();
+            Btn_pencil = new Button();
+            Btn_color = new Button();
+            pic_color = new Button();
+            pic = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pic).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveBorder;
+            panel1.Controls.Add(Btn_clear);
+            panel1.Controls.Add(Btn_load);
+            panel1.Controls.Add(Btn_save);
+            panel1.Controls.Add(Btn_line);
+            panel1.Controls.Add(Btn_rect);
+            panel1.Controls.Add(Btn_ellipse);
+            panel1.Controls.Add(Btn_eraser);
+            panel1.Controls.Add(Btn_pencil);
+            panel1.Controls.Add(Btn_color);
+            panel1.Controls.Add(pic_color);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(864, 119);
+            panel1.TabIndex = 0;
+            // 
+            // Btn_clear
+            // 
+            Btn_clear.BackColor = SystemColors.AppWorkspace;
+            Btn_clear.FlatAppearance.MouseDownBackColor = Color.Maroon;
+            Btn_clear.FlatAppearance.MouseOverBackColor = Color.FromArgb(128, 64, 64);
+            Btn_clear.ImageAlign = ContentAlignment.TopRight;
+            Btn_clear.Location = new Point(666, 31);
+            Btn_clear.Name = "Btn_clear";
+            Btn_clear.Size = new Size(48, 44);
+            Btn_clear.TabIndex = 9;
+            Btn_clear.Text = "Clear";
+            Btn_clear.UseVisualStyleBackColor = false;
+            Btn_clear.Click += Btn_clear_Click;
+            // 
+            // Btn_load
+            // 
+            Btn_load.BackColor = SystemColors.AppWorkspace;
+            Btn_load.FlatAppearance.MouseDownBackColor = Color.Maroon;
+            Btn_load.FlatAppearance.MouseOverBackColor = Color.FromArgb(128, 64, 64);
+            Btn_load.ImageAlign = ContentAlignment.TopRight;
+            Btn_load.Location = new Point(612, 31);
+            Btn_load.Name = "Btn_load";
+            Btn_load.Size = new Size(48, 44);
+            Btn_load.TabIndex = 8;
+            Btn_load.Text = "Load";
+            Btn_load.UseVisualStyleBackColor = false;
+            Btn_load.Click += Btn_load_Click;
+            // 
+            // Btn_save
+            // 
+            Btn_save.BackColor = SystemColors.AppWorkspace;
+            Btn_save.FlatAppearance.MouseDownBackColor = Color.Maroon;
+            Btn_save.FlatAppearance.MouseOverBackColor = Color.FromArgb(128, 64, 64);
+            Btn_save.ImageAlign = ContentAlignment.TopRight;
+            Btn_save.Location = new Point(558, 31);
+            Btn_save.Name = "Btn_save";
+            Btn_save.Size = new Size(48, 44);
+            Btn_save.TabIndex = 7;
+            Btn_save.Text = "Save";
+            Btn_save.UseVisualStyleBackColor = false;
+            Btn_save.Click += Btn_save_Click;
+            // 
+            // Btn_line
+            // 
+            Btn_line.BackColor = SystemColors.AppWorkspace;
+            Btn_line.Cursor = Cursors.Hand;
+            Btn_line.FlatAppearance.MouseDownBackColor = Color.Maroon;
+            Btn_line.FlatAppearance.MouseOverBackColor = Color.FromArgb(128, 64, 64);
+            Btn_line.Image = Properties.Resources.line;
+            Btn_line.ImageAlign = ContentAlignment.TopRight;
+            Btn_line.Location = new Point(446, 31);
+            Btn_line.Name = "Btn_line";
+            Btn_line.Size = new Size(48, 44);
+            Btn_line.TabIndex = 6;
+            Btn_line.UseVisualStyleBackColor = false;
+            Btn_line.Click += Btn_line_Click;
+            // 
+            // Btn_rect
+            // 
+            Btn_rect.BackColor = SystemColors.ActiveBorder;
+            Btn_rect.Cursor = Cursors.Hand;
+            Btn_rect.FlatAppearance.MouseDownBackColor = Color.Maroon;
+            Btn_rect.FlatAppearance.MouseOverBackColor = Color.FromArgb(128, 64, 64);
+            Btn_rect.Image = Properties.Resources.rectangle;
+            Btn_rect.ImageAlign = ContentAlignment.TopRight;
+            Btn_rect.Location = new Point(392, 31);
+            Btn_rect.Name = "Btn_rect";
+            Btn_rect.Size = new Size(48, 44);
+            Btn_rect.TabIndex = 5;
+            Btn_rect.UseVisualStyleBackColor = false;
+            Btn_rect.Click += Btn_rect_Click;
+            // 
+            // Btn_ellipse
+            // 
+            Btn_ellipse.BackColor = SystemColors.AppWorkspace;
+            Btn_ellipse.Cursor = Cursors.Hand;
+            Btn_ellipse.FlatAppearance.MouseDownBackColor = Color.Maroon;
+            Btn_ellipse.FlatAppearance.MouseOverBackColor = Color.FromArgb(128, 64, 64);
+            Btn_ellipse.Image = Properties.Resources.circle;
+            Btn_ellipse.ImageAlign = ContentAlignment.TopRight;
+            Btn_ellipse.Location = new Point(338, 31);
+            Btn_ellipse.Name = "Btn_ellipse";
+            Btn_ellipse.Size = new Size(48, 44);
+            Btn_ellipse.TabIndex = 4;
+            Btn_ellipse.UseVisualStyleBackColor = false;
+            Btn_ellipse.Click += Btn_ellipse_Click;
+            // 
+            // Btn_eraser
+            // 
+            Btn_eraser.BackColor = SystemColors.AppWorkspace;
+            Btn_eraser.Cursor = Cursors.Hand;
+            Btn_eraser.FlatAppearance.MouseDownBackColor = Color.Maroon;
+            Btn_eraser.FlatAppearance.MouseOverBackColor = Color.FromArgb(128, 64, 64);
+            Btn_eraser.Image = Properties.Resources.eraser;
+            Btn_eraser.ImageAlign = ContentAlignment.MiddleRight;
+            Btn_eraser.Location = new Point(284, 31);
+            Btn_eraser.Name = "Btn_eraser";
+            Btn_eraser.Size = new Size(48, 44);
+            Btn_eraser.TabIndex = 3;
+            Btn_eraser.UseVisualStyleBackColor = false;
+            Btn_eraser.Click += Btn_eraser_Click;
+            // 
+            // Btn_pencil
+            // 
+            Btn_pencil.BackColor = SystemColors.AppWorkspace;
+            Btn_pencil.Cursor = Cursors.Hand;
+            Btn_pencil.FlatAppearance.MouseDownBackColor = Color.Maroon;
+            Btn_pencil.FlatAppearance.MouseOverBackColor = Color.FromArgb(128, 64, 64);
+            Btn_pencil.Image = Properties.Resources.pencil;
+            Btn_pencil.ImageAlign = ContentAlignment.TopRight;
+            Btn_pencil.Location = new Point(230, 31);
+            Btn_pencil.Name = "Btn_pencil";
+            Btn_pencil.Size = new Size(48, 44);
+            Btn_pencil.TabIndex = 2;
+            Btn_pencil.UseVisualStyleBackColor = false;
+            Btn_pencil.Click += Btn_pencil_Click;
+            // 
+            // Btn_color
+            // 
+            Btn_color.BackColor = SystemColors.AppWorkspace;
+            Btn_color.Cursor = Cursors.Hand;
+            Btn_color.FlatAppearance.MouseDownBackColor = Color.Maroon;
+            Btn_color.FlatAppearance.MouseOverBackColor = Color.FromArgb(128, 64, 64);
+            Btn_color.Image = Properties.Resources.color;
+            Btn_color.ImageAlign = ContentAlignment.MiddleRight;
+            Btn_color.Location = new Point(176, 31);
+            Btn_color.Name = "Btn_color";
+            Btn_color.Size = new Size(48, 44);
+            Btn_color.TabIndex = 1;
+            Btn_color.UseVisualStyleBackColor = false;
+            Btn_color.Click += Btn_color_Click;
+            // 
+            // pic_color
+            // 
+            pic_color.BackColor = Color.White;
+            pic_color.Location = new Point(119, 27);
+            pic_color.Name = "pic_color";
+            pic_color.Size = new Size(51, 52);
+            pic_color.TabIndex = 0;
+            pic_color.UseVisualStyleBackColor = false;
+            // 
+            // pic
+            // 
+            pic.BackColor = Color.White;
+            pic.Dock = DockStyle.Fill;
+            pic.Location = new Point(0, 119);
+            pic.Name = "pic";
+            pic.Size = new Size(864, 392);
+            pic.TabIndex = 1;
+            pic.TabStop = false;
+            pic.Paint += pic_Paint;
+            pic.MouseDown += pic_MouseDown;
+            pic.MouseMove += pic_MouseMove;
+            pic.MouseUp += pic_MouseUp;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(864, 511);
+            Controls.Add(pic);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Paint";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pic).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private PictureBox pic;
+        private Button Btn_color;
+        private Button Pic_Color;
+        private Button Btn_eraser;
+        private Button Btn_pencil;
+        private Button Btn_line;
+        private Button Btn_rect;
+        private Button Btn_ellipse;
+        private Button Btn_load;
+        private Button Btn_save;
+        private Button Btn_clear;
+        private Button pic_Color;
+        private Button pic_color;
+    }
+}
---
+++ SuperPaintApp/Form1.cs
@@ -0,0 +1,191 @@
+using System.Drawing.Imaging;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+namespace SuperPaintApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Width = 950;
+            this.Height = 700;
+            bm = new Bitmap(pic.Width, pic.Height);
+            g = Graphics.FromImage(bm);
+            g.Clear(Color.White);
+            pic.Image = bm;
+        }
+
+
+        Bitmap bm;
+        Graphics g;
+        bool paint = false;
+        Point px, py;
+        Pen p = new Pen(Color.Black, 1);
+        Pen erase = new Pen(Color.White, 10);
+        int index;
+        int x, y, sX, sY, cX, cY;
+
+        ColorDialog cd = new ColorDialog();
+        Color new_color;
+
+        private void pic_MouseDown(object sender, MouseEventArgs e)
+        {
+            paint = true;
+            py = e.Location;
+
+            cX = e.X;
+            cY = e.Y;
+
+        }
+
+        private void pic_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (paint)
+            {
+
+                if (index == 1)
+                {
+                    px = e.Location;
+                    g.DrawLine(p, px, py);
+                    py = px;
+
+                }
+                if (index == 2)
+                {
+                    px = e.Location;
+                    g.DrawLine(erase, px, py);
+                    py = px;
+
+                }
+            }
+            pic.Refresh();
+
+            x = e.X;
+            y = e.Y;
+            sX = e.X - cX;
+            sY = e.Y - cY;
+
+
+        }
+
+        private void pic_MouseUp(object sender, MouseEventArgs e)
+        {
+            paint = false;
+
+            sX = x - cX;
+            sY = y - cY;
+
+            if (index == 3)
+            {
+                g.DrawEllipse(p, cX, cY, sX, sY);
+            }
+            if (index == 4)
+            {
+                g.DrawRectangle(p, cX, cY, sX, sY);
+            }
+            if (index == 5)
+            {
+                g.DrawLine(p, cX, cY, x, y);
+            }
+
+        }
+
+        private void Btn_pencil_Click(object sender, EventArgs e)
+        {
+            index = 1;
+        }
+
+        private void Btn_eraser_Click(object sender, EventArgs e)
+        {
+            index = 2;
+        }
+
+        private void Btn_ellipse_Click(object sender, EventArgs e)
+        {
+            index = 3;
+        }
+
+        private void Btn_rect_Click(object sender, EventArgs e)
+        {
+            index = 4;
+        }
+
+        private void Btn_line_Click(object sender, EventArgs e)
+        {
+            index = 5;
+        }
+
+        private void pic_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            if (paint)
+            {
+                if (index == 3)
+                {
+                    g.DrawEllipse(p, cX, cY, sX, sY);
+                }
+                if (index == 4)
+                {
+                    g.DrawRectangle(p, cX, cY, sX, sY);
+                }
+                if (index == 5)
+                {
+                    g.DrawLine(p, cX, cY, x, y);
+                }
+
+            }
+        }
+
+        private void Btn_clear_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            pic.Image = bm;
+            index = 0;
+        }
+
+        private void Btn_color_Click(object sender, EventArgs e)
+        {
+            cd.ShowDialog();
+            new_color = cd.Color;
+            pic_color.BackColor = cd.Color;
+            p.Color = cd.Color;
+
+        }
+
+        private void Btn_save_Click(object sender, EventArgs e)
+        {
+            var sfd = new SaveFileDialog();
+            sfd.Filter = "Image(*.jpg)|*.jpg|(*.*|*.*'";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap btm = bm.Clone(new Rectangle(0, 0, pic.Width, pic.Height), bm.PixelFormat);
+                btm.Save(sfd.FileName, ImageFormat.Jpeg);
+            }
+
+        }
+
+        private void Btn_load_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Filter = "Image(*.jpg)|*.jpg|(*.*|*.*'";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+
+                    Bitmap bitmap = new Bitmap(ofd.FileName);
+                    pic.Image = bitmap;
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Error loading image: " + ex.Message);
+                }
+                
+            }
+
+        }
+    }
+}
